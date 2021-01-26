7ddf3bbfc12f0684c9cb0c51ae839411e75b5904
Erik Miers <erik.miers@gempa.de>
2021-01-26 13:06:18 +0100

    removed build folder

--- build/Desktop_Qt_5_15_0_GCC_64bit-Debug/CMakeFiles/Timer_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Timer_autogen"
-  "Timer_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Timer_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
